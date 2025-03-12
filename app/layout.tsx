eb8169239d53ff43d13d0304fcdb85c88065203f
Han <windzhaohan@gmail.com>
2025-03-12 13:41:07 +0100

    Change default agent to galadriel, update page meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,9 @@ import { ThemeProvider } from '@/components/theme-provider';
 import './globals.css';
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://chat.vercel.ai'),
-  title: 'Next.js Chatbot Template',
-  description: 'Next.js chatbot template using the AI SDK.',
+  metadataBase: new URL('https://chat.galadriel.com'),
+  title: 'Galadriel Chat',
+  description: 'Galadriel Chat',
 };
 
 export const viewport = {
